e43614ddbbdec7e488ea98c629c720ef856fad71
LAlinaV <144618212+LAlinaV@users.noreply.github.com>
2024-04-29 18:41:31 +0300

    Add files via upload

---
+++ lab5/Hignt.cpp
@@ -0,0 +1,87 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    
+    HANDLE hReadPipe, hWritePipe;
+    HANDLE hEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"Event");
+    if (!hEvent) {
+        cout << "Event was not read";
+    }
+    sscanf(argv[0], "%p", &hWritePipe);
+    sscanf(argv[1], "%p", &hReadPipe);
+    
+    int size;
+
+    DWORD dwBytesWrite, dwBytesRead;
+
+    
+    if (!ReadFile(hReadPipe, &size, sizeof(int), &dwBytesRead, NULL)) {
+        cout << "array  read is failed " << endl;
+        return GetLastError();
+    }
+
+    __int16 N;
+    cout << "Enter N ";
+    cin >> N;
+
+    // Dynamic allocation of array
+    __int16* arr = new __int16[size];
+    srand(time(0)); // инициализация генератора случайных чисел
+    int colvo = 0;
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+        // генерация случайного числа от 0 до 99
+    }
+
+    cout << "Generated Array:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+   vector<__int16> res(0);
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > N) {
+            colvo++;
+            res.push_back(arr[i]);
+        }
+    }
+    int new_size = colvo;
+    __int16* arr1 = new __int16[new_size];
+
+    
+        for (int i = 0; i < new_size; ++i) {
+            cout << res[i] << " ";
+            arr1[i] = res[i];
+        }
+    
+    cout << endl;
+    SetEvent(hEvent);
+    
+
+    if (!WriteFile(hWritePipe, &new_size, sizeof(int), &dwBytesWrite, NULL)) {
+        cout << "Write of new size failed\n";
+        return GetLastError();
+    }
+    if (!WriteFile(hWritePipe, arr1, new_size * sizeof(__int16), &dwBytesWrite, NULL)) {
+        cout << "Write of new array failed\n";
+        return GetLastError();
+    }
+
+    _cprintf("\nTo exit press any key ");
+    _getch();
+
+    delete[] arr;
+    delete[] arr1;
+    CloseHandle(hWritePipe);
+    CloseHandle(hReadPipe);
+
+    return 0;
+}
---
+++ lab5/Server.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	cout << "Enter the length of the array: ";
+	int arrayLength;
+	cin >> arrayLength;
+
+	HANDLE hWritePipe, hReadPipe, hEvent, hInheritWritePipe, hInheritReadPipe;
+	hEvent = CreateEvent(nullptr, FALSE, FALSE, L"Event");
+	if (!hEvent) {
+		cout << "-Server-\n Event was not created";
+		return 3;
+	}
+
+	SECURITY_ATTRIBUTES saAttr;
+	saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+	saAttr.bInheritHandle = FALSE;
+	saAttr.lpSecurityDescriptor = NULL;
+
+	// Create pipes
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &saAttr, 0)) {
+		cout << "Create pipe failed" << endl;
+		return GetLastError();
+	}
+	if (!DuplicateHandle(
+
+		GetCurrentProcess(), // дескриптор текущего процесса
+		hWritePipe, // исходный дескриптор канала
+		GetCurrentProcess(), // дескриптор текущего процесса
+		&hInheritWritePipe, // новый дескриптор канала
+		0, // этот параметр игнорируется
+		TRUE, // новый декскриптор наследуемый
+		DUPLICATE_SAME_ACCESS))// доступ не изменяем
+
+	{
+		_cputs("Duplicate handle failed.\n");
+		_cputs("Press any key to finish.\n");
+		_getch();
+		return GetLastError();
+	}
+	// закрываем ненужный дескриптор
+	CloseHandle(hWritePipe);
+
+	if (!DuplicateHandle(
+
+		GetCurrentProcess(), // дескриптор текущего процесса
+		hReadPipe, // исходный дескриптор канала
+		GetCurrentProcess(), // дескриптор текущего процесса
+		&hInheritReadPipe, // новый дескриптор канала
+		0, // этот параметр игнорируется
+		TRUE, // новый декскриптор наследуемый
+		DUPLICATE_SAME_ACCESS))// доступ не изменяем
+
+	{
+		_cputs("Duplicate handle failed.\n");
+		_cputs("Press any key to finish.\n");
+		_getch();
+		return GetLastError();
+	}
+	// закрываем ненужный дескриптор
+	CloseHandle(hReadPipe);
+
+	DWORD dwBytesWrite;
+	// Write array data to the pipe
+	if (!WriteFile(hInheritWritePipe, &arrayLength, sizeof(int), &dwBytesWrite, NULL)) {
+		cout << "Write of length failed\n" << GetLastError();
+		return GetLastError();
+	}
+
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	wchar_t* lpszComLine = new wchar_t[80];
+	wsprintf(lpszComLine, L"%p %p", hInheritWritePipe, hInheritReadPipe);
+	ZeroMemory(&si, sizeof(si));
+	ZeroMemory(&pi, sizeof(pi));
+	si.cb = sizeof(si);
+
+	
+	if (!CreateProcess(L"Hignt.exe", lpszComLine,
+		NULL, NULL, TRUE,
+		CREATE_NEW_CONSOLE,
+		NULL, NULL,
+		&si, &pi))
+	{
+		cout << "Hignt.exe was not launched.\n";
+		return GetLastError();
+	}
+
+
+	WaitForSingleObject(hEvent, INFINITE);
+
+	int new_size;
+	DWORD byteRwad;
+	if (!ReadFile(hInheritReadPipe, &new_size, sizeof(int), &byteRwad, NULL))
+	{
+		cout << "New size read is failed " << endl;
+		return GetLastError();
+	}
+	__int16* arr1 = new __int16[new_size];
+	if (!ReadFile(hInheritReadPipe, arr1, sizeof(__int16) * new_size, &byteRwad, NULL))
+	{
+		cout << "New array read is failed " << endl;
+		return GetLastError();
+	}
+
+
+	for (int i = 0; i < new_size; i++)
+	{
+		cout << arr1[i] << " ";
+	}
+
+	CloseHandle(hInheritReadPipe);
+	CloseHandle(hInheritWritePipe);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+
+	delete[] arr1;
+
+	return 0;
+}
